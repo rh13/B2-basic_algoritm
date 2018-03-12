d63e977f9c91782a07925f4f63cbebe8a3abee8f
rh13 <rhlikhon13@gmail.com>
2018-03-13 00:23:19 +0600

    edit solve check prime number problem

--- check_prime_number.py
+++ check_prime_number.py
@@ -1,11 +1,10 @@
-flag=1
-i=2
-n=input()
-while i<(n/2):
- if (n%i==0):
-   flag=0
- i=i+1
-if flag==0:
-  print(n,"is not prime")
-else:
- print(n,"is prime")
+
+x=int(input())
+def is_prime(x):
+  for n in range(2,x):
+      if x%n==0:
+         return False
+  return True
+if is_prime(x):
+ print(x, "is a prime number")
+else: print(x, "is not a prime number")
